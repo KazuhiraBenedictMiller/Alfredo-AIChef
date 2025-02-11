3ac66ac11594bb976778337cef8fadaf03b5f65a
Marco-b-opit <182032749+Marco-b-opit@users.noreply.github.com>
2025-02-11 23:58:19 +0100

    add an async server page func

--- alfredo-aichef/src/app/auth/login/page.tsx
+++ alfredo-aichef/src/app/auth/login/page.tsx
@@ -1,7 +1,8 @@
 import { Box } from '@mui/material';
 import { SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs';
 
-export default function Login() {
+export default async function Login() {
+  console.log('This is server side logic');
   return (
     <Box
       height={'100%'}
