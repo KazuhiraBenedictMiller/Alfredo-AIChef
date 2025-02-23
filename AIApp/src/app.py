4802f90e024d1b0240850f66105f2b95dcb47b2e
KazuhiraBenedictMiller <aaserravalle.py@gmail.com>
2025-02-23 10:50:28 +0100

    Enhanced Dev Local Environment

--- AIApp/src/app.py
+++ AIApp/src/app.py
@@ -13,6 +13,7 @@ from fastapi.security.api_key import APIKeyHeader
 from starlette.status import HTTP_403_FORBIDDEN
 from pydantic import BaseModel
 from typing import Union, Optional, Dict, List
+from datetime import datetime
 import os
 import json
 
@@ -21,7 +22,6 @@ app = FastAPI()
 # Define the API key header
 api_key_header = APIKeyHeader(name = "Auth")
 
-# List of valid API keys (in a real application, store these securely)
 # VALID_API_KEYS = {"mattbelcher", "aaserravalle"}
 
 VALID_API_KEY = os.environ.get("ACTUAL_API_KEY")
@@ -44,31 +44,61 @@ async def read_root():
 # Status endpoint
 @app.get("/status/", dependencies = [Depends(validate_api_key)])
 async def get_status():
-    return {"status": "Healthy", "message": "API is running fine."}
+    return {"status": "Healthy", "message": "API is running fine.", "timestamp": datetime.now().isoformat()}
 
 class IngredientDetails(BaseModel):
     Name: str
     Quantity: Union[float, int]
     Unit: str
 
-class Interaction(BaseModel):
-    UserPrompt: Optional[str] = None
-    AIPrompt: Optional[str] = None
-
 class Input(BaseModel):
     Ingredients: List[IngredientDetails]
     SystemPrompt: Optional[str] = None
     Interactions: List[Dict[str, str]] = {}
 
-@app.post("/inference/", dependencies = [Depends(validate_api_key)])
-async def predict(input: Input):
+'''
+@app.post("/mirror/", dependencies = [Depends(validate_api_key)])
+async def ReturnInputs(input: Input):
     # Implement your prediction logic here
     Ings = "\n".join([f"{x.Name}: {x.Quantity} {x.Unit}" for x in input.Ingredients])
     systemPrompt = input.SystemPrompt if input.SystemPrompt else "Default System Prompt"
-    interactionsString = ""
+    interactionsString = "\n".join(f"{Persona}: {Prompt}" for interaction in input.Interactions for Persona, Prompt in interaction.items())
+    Result = f"""Received the Following Ingredients: {Ings}
+    SystemPrompt: {systemPrompt}
+    Interactions: {interactionsString}
+    """
+    return {"Prediction": Result}
+'''
+
+@app.post("/mirror/", dependencies = [Depends(validate_api_key)])
+async def ReturnInputs(input: Input):
+    Ings = []
+    for x in input.Ingredients:
+        Ings.append({"Name": x.Name, "Quantity": x.Quantity, "Unit": x.Unit})
+
+    Chat = []
     for interaction in input.Interactions:
         for Persona, Prompt in interaction.items():
-            interactionsString += f"{Persona}: {Prompt}\n"
+            Chat.append({f"{Persona}": f"{Prompt}"})
+    
+    Results = {
+        "ReceivedInputs": {
+            "Ingredients": Ings,
+            "Messages": {
+                "SystemPrompt": input.SystemPrompt,
+                "Chat": Chat
+            }
+        }
+    }
+
+    return Results
+
+@app.post("/inference/", dependencies = [Depends(validate_api_key)])
+async def Predict(input: Input):
+    # Implement your prediction logic here
+    Ings = "\n".join([f"{x.Name}: {x.Quantity} {x.Unit}" for x in input.Ingredients])
+    systemPrompt = input.SystemPrompt if input.SystemPrompt else "Default System Prompt"
+    interactionsString = "\n".join(f"{Persona}: {Prompt}" for interaction in input.Interactions for Persona, Prompt in interaction.items())
     Result = f"""Received the Following Ingredients: {Ings}
     SystemPrompt: {systemPrompt}
     Interactions: {interactionsString}
