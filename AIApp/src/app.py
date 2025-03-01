ba308a9e673fa8c25ccc0d578b6db0117659391b
KazuhiraBenedictMiller <aaserravalle.py@gmail.com>
2025-03-01 22:59:51 +0100

    Added Parsing Output for Recipes

--- AIApp/src/app.py
+++ AIApp/src/app.py
@@ -11,12 +11,14 @@ curl -X GET "http://3.72.247.90:8000" \
 from fastapi import FastAPI, Depends, HTTPException, Security
 from fastapi.security.api_key import APIKeyHeader
 from starlette.status import HTTP_403_FORBIDDEN
-from pydantic import BaseModel
-from typing import Union, Optional, Dict, List
+from pydantic import BaseModel, Field
+from typing import Union, Optional, Literal, Dict, List
 from langchain_google_genai import ChatGoogleGenerativeAI
-# More Langchian Packages
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, AIMessagePromptTemplate
 import os
+import json
 from datetime import datetime
 
 app = FastAPI()
@@ -95,6 +97,15 @@ async def ReturnInputs(input: Input):
     }
 
     return Results
+class PrepCookTime(BaseModel):
+    Preparation: str = Field(description = "Preparation Time of the Recipe")
+    Cooking: str = Field(description = "Cooking Time of the Recipe")
+
+class RecipeOutput(BaseModel):
+    RecipeName: str = Field(description = "Name of the Recipe")
+    Ingredients: List[str] = Field(description = "List of Available Ingredients")
+    ServingTime: PrepCookTime = Field(description = "Preparation and Cooking Time for the Recipe")
+    CookingInstructions: str = Field(description = "Cooking Instructions for the Recipe")
 
 @app.post("/inference/", dependencies = [Depends(validate_api_key)])
 async def Predict(input: Input):
@@ -144,17 +155,20 @@ async def Predict(input: Input):
         max_retries = 3,
     )
 
+    # Defining the Output Parser for the Recipe
+    RecipeParser = PydanticOutputParser(pydantic_object = RecipeOutput)
+
     # Creating an Initial ChatPromptTemplate
-    SystemPrompt = "You are a Very Cool AI Chef named {AIName} with {Roots} roots"
+    SystemPrompt = "You are a Very Cool AI Chef named {AIName} with {Roots} roots. Give a recipe to the user following the instructions and output the answer as a JSON object that follows the schema provided. Do not include any additional text outside of the JSON object."
     SystemMessagePrompt = SystemMessagePromptTemplate.from_template(SystemPrompt)
-    HumanPrompt = "{Input}"
+    HumanPrompt = "{Input}\n{FormatInstructions}"
     HumanMessagePrompt = HumanMessagePromptTemplate.from_template(HumanPrompt)
 
     # Actual Chat that will get Extended
     Chat = ChatPromptTemplate.from_messages([SystemMessagePrompt, HumanMessagePrompt])
 
     # Defining the Chain
-    Chain = Chat | LLM
+    Chain = Chat | LLM | RecipeParser
 
     # Generating the Recipe
     Recipe = Chain.invoke(
@@ -162,14 +176,17 @@ async def Predict(input: Input):
             "AIName": "Alfredo",
             "Roots": "Italian",
             "Input": "I have 2 Bananas, 200 Grams of Chocolate, and Some Milk, can you suggest a cool recipe for me to cook?",
+            "FormatInstructions": RecipeParser.get_format_instructions()
         }
     )
 
-    # Appening AI Response and Asking for Refinement
-    AIRecipe = Recipe.content
-    AIMessagePrompt = AIMessagePromptTemplate.from_template(AIRecipe)
-    Chat.messages.append(AIMessagePrompt)
+    # Appending AI Response and Asking for Refinement
+    AIRecipe = Recipe.json()
+    #AIMessagePrompt = AIMessagePromptTemplate.from_template(AIRecipe)
+    #Chat.messages.append(AIMessagePrompt)
+    Chat.messages.append(AIMessage(content=AIRecipe)) # append AIMessage instead
 
+    # Checking that the Answers Only has the Requested Ingredients
     HumanPrompt = "I actually found 400 Grams of Strawberries, can you make a variations including them?"
     HumanMessagePrompt = HumanMessagePromptTemplate.from_template(HumanPrompt)
     Chat.messages.append(HumanMessagePrompt)
@@ -180,11 +197,8 @@ async def Predict(input: Input):
             "AIName": "Alfredo",
             "Roots": "Italian",
             "Input": "I have 2 Bananas, 200 Grams of Chocolate, and Some Milk, can you suggest a cool recipe for me to cook?",
+            "FormatInstructions": RecipeParser.get_format_instructions()
         }
     )
 
-    return Recipe, NewRecipe
-
-
-
-
+    return  Recipe, NewRecipe
