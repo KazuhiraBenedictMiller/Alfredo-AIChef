f381aebf5f0eebbb15d026aac2a4a81a025b06ef
OneByteToRuleThemAll <matthew.b@students.opit.com>
2025-03-23 22:56:34 +0100

    updated authcheck process

--- alfredo-aichef/src/app/dashboard/page.tsx
+++ alfredo-aichef/src/app/dashboard/page.tsx
@@ -1,17 +1,26 @@
 import { RecipeBuilderV2 } from '@/components/feature/recipe-builder-v2';
 import { Grid2 } from '@mui/material';
+import AuthCheck from '@/components/common/AuthCheck';
+import { auth } from '@clerk/nextjs/server'
+import { RedirectToSignIn } from '@clerk/nextjs'
 
 export default async function Dashboard() {
+  const user = await auth();
+  if (!user) {
+    return <RedirectToSignIn />
+  }
   return (
-    <Grid2
-      container
-      height={'100%'}
-      justifyContent={'center'}
-      px={'64px'}
-      py={'96px'}
-      spacing={2}
-    >
-      <RecipeBuilderV2 />
-    </Grid2>
+    <AuthCheck>
+      <Grid2
+        container
+        height={'100%'}
+        justifyContent={'center'}
+        px={'64px'}
+        py={'96px'}
+        spacing={2}
+      >
+        <RecipeBuilderV2 />
+      </Grid2>
+    </AuthCheck>
   );
 }
